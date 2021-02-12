585f2094146adf907c96f239595c10db7e563f24
Hsieh Ching Hsiang <vita_hsieh@morrisonexpress.com>
2021-02-12 17:32:43 +0800

    add Navigation component

--- src/App/commons/Navigation/index.js
+++ src/App/commons/Navigation/index.js
@@ -1,31 +1,59 @@
-import React, { useMemo } from 'react'
-import { Navbar, Form, Button } from 'react-bootstrap'
-import { Link, useLocation } from 'react-router-dom'
+import React from 'react'
+import { Container, Button, Row, Col } from 'react-bootstrap'
+import { Link, useLocation, useHistory } from 'react-router-dom'
 
 import './styles.scss'
 
 const Navigation = () => {
   const location = useLocation()
+  const history = useHistory()
 
   if (location.pathname === '/signUp') return <></>
   if (location.pathname === '/login') return <></>
 
+  const handleGoToSignUpPage = () => {
+    history.push('/signUp')
+  }
+
   return (
-    <Navbar bg='primary' variant='dark' sticky='top' className='mb-5'>
-      <Navbar.Brand>
-        <Link to='/'>Home</Link>
-      </Navbar.Brand>
-      <Navbar.Collapse className='justify-content-end mr-3'>
-        <Navbar.Text>
-          111
-        </Navbar.Text>
-      </Navbar.Collapse>
-      <Form inline>
-        <Button variant='outline-light'>
-          111
-        </Button>
-      </Form>
-    </Navbar>
+    <Container>
+      <header>
+        <Row className='align-items-center'>
+          <Col xl={4} xs={3}>
+            <div className='header__home'>
+              <p><Link to='/technologyList'>Home</Link></p>
+            </div>
+          </Col>
+          <Col xl={4} xs={6}>
+            <div className='header__logo'>
+              <h3>Technology</h3>
+            </div>
+          </Col>
+          <Col xl={4} xs={3}>
+            <div className='header__login'>
+              <Button variant='outline-secondary' size='sm' onClick={handleGoToSignUpPage}>Sign up</Button>
+            </div>
+          </Col>
+        </Row>
+      </header>
+      <div className='nav-scroller py-1 mb-2'>
+        <nav className='nav d-flex justify-content-between'>
+          <Link className='p-2 text-muted' to='/technologyList'>Technology List</Link>
+          <Link className='p-2 text-muted' to='/userList'>User List</Link>
+          {/* <a className='p-2 text-muted' href='#'>U.S.</a>
+          <a className='p-2 text-muted' href='#'>Technology</a>
+          <a className='p-2 text-muted' href='#'>Design</a>
+          <a className='p-2 text-muted' href='#'>Culture</a>
+          <a className='p-2 text-muted' href='#'>Business</a>
+          <a className='p-2 text-muted' href='#'>Politics</a>
+          <a className='p-2 text-muted' href='#'>Opinion</a>
+          <a className='p-2 text-muted' href='#'>Science</a>
+          <a className='p-2 text-muted' href='#'>Health</a>
+          <a className='p-2 text-muted' href='#'>Style</a>
+          <a className='p-2 text-muted' href='#'>Travel</a> */}
+        </nav>
+      </div>
+    </Container>
   )
 }
 
